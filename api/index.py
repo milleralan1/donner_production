4d011d23d025e558cc65a937701f9a274c707f3b
Ashish Garg <gargashish11@gmail.com>
2026-09-09 19:54:10 +0530

    updated client

--- api/index.py
+++ api/index.py
@@ -2,11 +2,19 @@ from fastapi import FastAPI  # type: ignore
 from fastapi.responses import PlainTextResponse  # type: ignore
 from openai import OpenAI  # type: ignore
 
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+
+
 app = FastAPI()
 
 @app.get("/api", response_class=PlainTextResponse)
 def idea():
-    client = OpenAI()
+    client = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+        base_url=os.environ.get("OPENAI_BASE_URL")
+    )
     prompt = [{"role": "user", "content": "Come up with a new business idea for AI Agents"}]
-    response = client.chat.completions.create(model="gpt-5-nano", messages=prompt)
+    response = client.chat.completions.create(model="gemini-3.6-flash", messages=prompt)
     return response.choices[0].message.content
